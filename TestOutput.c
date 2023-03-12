2f2b1ec5fc226d7e277c7350221c345f61d74d56
zhenyuma <alexduck@qq.com>
2023-03-12 09:38:52 +0800

    new file:   .vscode/launch.json 	modified:   TestOutput.c 	modified:   TestOutput.exe 	new file:   output/TestOutput.exe

--- TestOutput.c
+++ TestOutput.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include"calculate.h"
 
 int main(){
 
+    double a ,b ,c;
+    a = 4;
+    b = 2;
+    c = 0;
 
     printf("hi zhenyu!\n");
+   // c = minus(a,b);
+    printf("%f\n",c);
 
     return 0;
 }
